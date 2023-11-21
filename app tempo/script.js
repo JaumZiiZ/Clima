9413e818b518d19e3ca7beb955da69f4958415c3
Iohannes Paulus <126628609+JaumZiiZ@users.noreply.github.com>
2023-11-21 11:38:02 -0300

    Add files via upload

---
+++ app tempo/script.js
@@ -0,0 +1,25 @@
+
+
+const key = "f50833c3847f54892ab52fc2cd0635e5"
+
+function colocarDadosNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = " Tempo em " + dados.name 
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather(0).description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+
+}  
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt-br&units=metric`).then( resposta => resposta.json())
+    
+    
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)    
+}
